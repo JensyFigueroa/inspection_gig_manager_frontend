8a1633e4fb3ef1e73fe84c9ab745e6d9e1544eb6
Jensy Figueroa <jensy1418@gmail.com>
2026-03-15 16:18:20 -0400

    Frontend readay for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import { useInactivityTimeout } from './hooks/useInactivityTimeout';
 import { toast } from 'sonner';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+console.log(BACKEND_URL,'APP')
 export const API = `${BACKEND_URL}/api`;
 
 export const authAxios = axios.create({
